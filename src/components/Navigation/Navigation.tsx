65bee93316bc28e762d91ae52a7f2a17a4eeba1c
Oscar Zhang <oscarzhang228@gmail.com>
2024-03-18 23:21:37 -0400

    remove scrollbar and add drawer component

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,19 +1,39 @@
 import MenuOpenIcon from "@mui/icons-material/MenuOpen";
 import Button from "@mui/material/Button";
+import { Drawer } from "@mui/material";
+import { useState } from "react";
 
 export default function Navigation() {
+  const [open, setOpen] = useState<boolean>(false);
+
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
+
   return (
     <>
-      <Button sx={navigationButtonStyle}>
+      <Button onClick={toggleDrawer} sx={navigationButtonStyle}>
         <MenuOpenIcon sx={menuIconStyle} />
       </Button>
+      <Drawer
+        open={open}
+        onClose={toggleDrawer}
+        variant="temporary"
+        sx={drawerStyle}
+      >
+        <section>Navigation</section>
+      </Drawer>
     </>
   );
 }
 
+const drawerStyle = {
+  maxWidth: "100vw",
+};
+
 const menuIconStyle = {
   color: "black",
-  fontSize: "2rem",
+  fontSize: "1.7rem",
 };
 const navigationButtonStyle = {
   backgroundColor: "e9e9e9",
