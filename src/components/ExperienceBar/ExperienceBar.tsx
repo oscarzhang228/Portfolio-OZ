2083db4485bb61913ef7946e485778104e7d01b5
Oscar Zhang <oscarzhang228@gmail.com>
2024-03-24 00:53:51 -0400

    change experience bar from hard coded to pulling from json

--- src/components/ExperienceBar/ExperienceBar.tsx
+++ src/components/ExperienceBar/ExperienceBar.tsx
@@ -1,8 +1,16 @@
 import styles from "./ExperienceBar.module.scss";
 import Card from "@mui/material/Card";
-import { CardContent, CardMedia } from "@mui/material";
+import { CardMedia } from "@mui/material";
 import fyvebyLogo from "../../assets/fyveby.png";
+import socwebLogo from "../../assets/socweb.jpg";
 import TechnologyChip from "../TechnologyChip/TechnologyChip";
+import experiences from "../../data/experience.json";
+
+const imageMap: any = {
+  fyvebyLogo: fyvebyLogo,
+  socwebLogo: socwebLogo,
+};
+
 /**
  * ExperienceBar component used to display the jobs I've had on the landing page
  * @returns ExperienceBar component
@@ -15,21 +23,23 @@ export default function ExperienceBar() {
       <h1 className={`${styles["ExperienceBar-Header"]} p-2 px-3`}>
         I've worked with:
       </h1>
-      <section className="p-3">
-        <Experience
-          title="Software Engineer Intern"
-          company="Fyve By"
-          description="An aviation startup in the ground damage prevention space creating a digital clone of hangars. I worked on the backend and front-end of their web application for managers to oversee scheduling, template hangars, and account/billing details. I also worked on the iOS application for crew moving planes to be able to see the created templates as well as for near-collision alarms when moving too close to another plane."
-          logo={fyvebyLogo}
-          technologies={[
-            "React",
-            "TypeScript",
-            "AWS Api Gateway",
-            "AWS DynamoDB",
-            "Swift",
-            "C#",
-          ]}
-        />
+      <section className="p-3 d-flex flex-column gap-2">
+        {experiences.map((experience: any, index: number) => {
+          return (
+            <section
+              className={`${styles["ExperienceBar-ExperienceContainer"]} `}
+            >
+              <Experience
+                key={index}
+                title={experience.title}
+                company={experience.company}
+                description={experience.description}
+                logo={imageMap[experience.logo]}
+                technologies={experience.technologies}
+              />
+            </section>
+          );
+        })}
       </section>
     </section>
   );
@@ -50,29 +60,27 @@ type ExperienceProps = {
 const Experience = (props: ExperienceProps) => {
   const { title, company, description, logo, technologies } = props;
   return (
-    <Card className="d-flex flex-column p-3">
-      <section className="d-flex gap-2 flex-wrap flex-md-nowrap justify-content-start">
-        <CardMedia
-          className={`${styles["Experience-Logo"]} d-flex justify-content-center flex-column align-items-center px-2 py-3 `}
-        >
-          <img src={logo} className="mw-100 mh-100"></img>
-        </CardMedia>
-        <CardContent className="d-flex flex-wrap align-items-center text-nowrap pb-0">
+    <Card className="container-flex p-3">
+      <section className="row">
+        <section className="col-lg-3 d-flex align-items-center gap-4 flex-wrap">
+          <CardMedia className={`${styles["Experience-Logo"]}`}>
+            <img src={logo} className="mh-100 mw-100"></img>
+          </CardMedia>
           <section>
             <h1 className={`${styles["Experience-Title"]}`}>{company}</h1>
             <h2 className={`${styles["Experience-Subtitle"]}`}>{title}</h2>
           </section>
-        </CardContent>
-        <CardContent className="py-1 d-flex align-items-center">
+        </section>
+        <section className="col-lg-9 py-1 d-flex flex-column justify-content-center gap-lg-2 gap-4">
           <p className={`${styles["Experience-Body"]} text-wrap mb-0`}>
             {description}
           </p>
-        </CardContent>
-      </section>
-      <section className="px-4 pb-2 d-lg-flex d-none gap-2 justify-content-end ">
-        {technologies?.map((tech, index) => {
-          return <TechnologyChip key={index} label={tech} />;
-        })}
+          <section className="px-4 pb-2 d-lg-flex d-none gap-2 justify-content-end ">
+            {technologies?.map((tech, index) => {
+              return <TechnologyChip key={index} label={tech} />;
+            })}
+          </section>
+        </section>
       </section>
     </Card>
   );
